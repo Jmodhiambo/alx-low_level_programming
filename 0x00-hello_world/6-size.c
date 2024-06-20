4af3d4bc1bd5f83839689d2c31f8d97c31c9a787
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-06-21 02:39:44 +0300

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * This program uses sizeof operator
- * The size of different types of variables
- *
- * Return - Always 0 (Success)
+ * Description: This program demonstrates the usage of the sizeof operator
+ *              to determine the size of different types of variables.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -14,11 +14,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(c));
-	printf("Size of a int: %d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %d byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
-	printf("Size of a float: %d byte(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of a int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+	printf("Size of a float: %lu byte(s)", sizeof(f));
 
 	return (0);
 }
