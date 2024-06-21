84066b97bacbf496dad978b024d187ce587e9c7e
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-06-21 03:56:08 +0300

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: this program prints to the standrd error
+ * A return value of 1 signifies a unseccessful running of the program
+ *
+ * Return: 1(signifies return error)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
