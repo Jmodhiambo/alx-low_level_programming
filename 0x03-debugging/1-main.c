d4a86bf62b6e5cc7b1d3fc447423b3669a2b9b8f
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-06-27 17:19:47 +0300

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i); /* There is no i++ to increment i */
-	}
+	/*while (i < 10)*/
+	/*{*/
+		/*putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
