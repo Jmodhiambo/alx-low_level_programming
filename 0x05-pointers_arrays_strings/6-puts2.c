01c1f6bcb219c898a308e1ea03884fe3337a1ed7
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-07-02 07:20:45 +0300

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,12 @@
 
 void puts2(char *str)
 {
-	int len = 0;
+	int i = 0;
 
-	while (str[len] != '\0' && str[len + 1] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[len]);
-		len += 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
