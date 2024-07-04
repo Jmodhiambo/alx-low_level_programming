cefe50e25790a1622b473116a2726ebd22d7cd50
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-07-05 00:31:42 +0300

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,51 @@
 #include "main.h"
 
 /**
-* cap_string - capitalizes all words of a string.
-* @str: The string.
+* cap_string - Capitalizes all words of a string.
+* @str: The string to be modified.
 *
-* Return: Captalized string.
+* Return: A pointer to the modified string.
 */
-
 char *cap_string(char *str)
 {
 	int i = 0;
+	int cap_next = 1;
+	char separators[] = " \t\n,;.!?\"(){}";
+	int j = 0;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ')
-			str[i + 1] = str[i + 1] - 32;
+		/* Check if the current character is a separator */
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j])
+			{
+				cap_next = 1;
+				break;
+			}
+		}
+
+		/* Capitalize the current character if needed */
+		if (cap_next && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+			cap_next = 0;
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			cap_next = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+		{
+			cap_next = 0;
+		}
+		else
+		{
+			cap_next = 1;
+		}
+
+		i++;
 	}
-	str[i] = '\0';
 
 	return (str);
-
 }
