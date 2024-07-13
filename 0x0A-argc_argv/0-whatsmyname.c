1148c039cdf667cb1393482a1af9a2ea0283c4e5
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-07-13 04:13:32 +0300

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* main - return the name of the file using argument.
+* @argc: Void
+* @argv: file name
+*
+* Return: Always 0 when successful
+*/
+
 int main(int argc, char *argv[])
 {
 	(void)argc;
