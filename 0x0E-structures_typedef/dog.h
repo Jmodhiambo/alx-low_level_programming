b1af3352acdc39a474763af0e5bc768730b281a0
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-07-23 16:56:17 +0300

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,14 +9,12 @@
 *
 * Description: Longer description
 */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
