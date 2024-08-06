da523c136cc45667529335bdbde6a7f6605915be
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-08-07 02:49:12 +0300

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+		return;
+
 	while (*head)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
-	free(*head);
 	*head = NULL;
 }
