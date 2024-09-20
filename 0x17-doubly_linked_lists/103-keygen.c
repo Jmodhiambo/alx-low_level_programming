382c0f96d09f4967104b651deab6dd31621aad3a
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-09-21 00:53:55 +0300

    Task 11

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,38 +1,31 @@
+/**
+ * keygen5 - Generates a key based on the provided username
+ * @username: the username to generate a key for
+ *
+ * Return: void (prints the key)
+ */
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-/**
-* main - Generates a key for the given username
-* @argc: argument count
-* @argv: argument vector
-*
-* Return: 0 on success
-*/
 int main(int argc, char *argv[])
 {
-	char *username;
-	int len, i;
-	unsigned int key;
+        if (argc != 2)
+        {
+                printf("Usage: %s username\n", argv[0]);
+                return (1);
+        }
 
-	if (argc != 2)
-	{
-		printf("Usage: %s <username>\n", argv[0]);
-		return (1);
-	}
+        char *username = argv[1];
+        int len = strlen(username);
+        int key = 0;
 
-	username = argv[1];
-	len = strlen(username);
-	key = 0;
+        for (int i = 0; i < len; i++)
+        {
+                key += username[i];
+        }
 
-	for (i = 0; i < len; i++)
-	{
-		key += username[i] * (i + 1);
-	}
+        key = (key ^ 0x4f) & 0xff;  // Hypothetical transformation
 
-	key = (key ^ 0xabcdef) & 0xffffffff;
-
-	printf("%u\n", key);
-
-	return (0);
+        printf("%d\n", key);
+        return (0);
 }
