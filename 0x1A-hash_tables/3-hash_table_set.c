dc044208b966d2d054231b514dbd42a52d0d229e
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-10-11 15:10:35 +0300

    Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* hash_table_set - Sets the node at the hash table.
+* @ht: The table to update with a new node
+* @key: The key of the new node
+* @value: The value of the new node
+*
+* Return: 1 on success and 0 incase of otherwise
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
