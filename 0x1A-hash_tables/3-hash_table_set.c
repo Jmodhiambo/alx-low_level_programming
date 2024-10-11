9937b6744e8a05d5fb224cba93463f6b368cac14
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-10-11 15:41:15 +0300

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,11 +14,27 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *temp;
 
 	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
+
+	/* Check if the key already exists and update it */
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)  /* Key found */
+		{
+			free(temp->value);       /* Free old value */
+			temp->value = strdup(value);  /* Assign new value */
+			if (temp->value == NULL)
+				return (0);       /* strdup failure */
+			return (1);              /* Successfully updated value */
+		}
+
+		temp = temp->next;
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
