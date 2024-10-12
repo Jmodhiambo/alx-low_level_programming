178d520c0f25a1a74d186d63178e750b7696aacc
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-10-12 21:28:21 +0300

    Task 7

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,199 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* shash_table_create - creates a hash table
+* @size: size of the hash table
+*
+* Return: a pointer to a newly created hash table
+*/
+
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *shash_table;
+	unsigned int i;
+
+	shash_table = malloc(sizeof(shash_table_t));
+	if (shash_table == NULL)
+		return (NULL);
+
+	shash_table->array = malloc(sizeof(shash_node_t *) * size);
+	if (shash_table->array == NULL)
+	{
+		free(shash_table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		shash_table->array[i] = NULL;
+	}
+	shash_table->size = size;
+	return (shash_table);
+}
+
+/**
+* shash_table_set - Sets the node at the hash table.
+* @ht: The table to update with a new node
+* @key: The key of the new node
+* @value: The value of the new node
+*
+* Return: 1 on success and 0 incase of otherwise
+*/
+
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+	shash_node_t *new_node, *temp, *prev;
+	unsigned int index;
+
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (temp->value == NULL)
+				return (0);
+			return (1);
+
+		}
+		temp = temp->snext;
+	}
+
+	new_node = malloc(sizeof(shash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+	/* Add the node to the hash table's linked list at the index */
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	/* Now, insert the new node into the sorted linked list */
+	if (ht->shead == NULL)
+	{
+		/* First element in the sorted list */
+		new_node->snext = NULL;
+		new_node->sprev = NULL;
+		ht->shead = new_node;
+		ht->stail = new_node;
+	}
+	else
+	{
+		temp = ht->shead;
+		prev = NULL;
+
+		/* Traverse the sorted list to find the correct position */
+		while (temp != NULL && strcmp(temp->key, key) < 0)
+		{
+			prev = temp;
+			temp = temp->snext;
+		}
+
+		/* Insert at the beginning of the list */
+		if (prev == NULL)
+		{
+			new_node->snext = ht->shead;
+			new_node->sprev = NULL;
+			ht->shead->sprev = new_node;
+			ht->shead = new_node;
+		}
+		/* Insert at the end of the list */
+		else if (temp == NULL)
+		{
+			new_node->snext = NULL;
+			new_node->sprev = ht->stail;
+			ht->stail->snext = new_node;
+			ht->stail = new_node;
+		}
+		/* Insert in the middle of the list */
+		else
+		{
+			new_node->snext = temp;
+			new_node->sprev = prev;
+			prev->snext = new_node;
+			temp->sprev = new_node;
+		}
+	}
+	return (1);
+}
+/**
+* shash_table_get - Retrieves a value associated with a key.
+* @ht: The hash table to look into.
+* @key: The key you are looking for.
+*
+* Return: The value associated with the element,
+*         or NULL if key couldn’t be found.
+*/
+
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	unsigned int index;
+	shash_node_t *node;
+
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+		return ("(null)");
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+	return ("(null)");
+}
+
+/**
+* shash_table_print - Prints the key-value pairs in a hash table.
+* @ht: The hash table.
+*
+* Return: void.
+*/
+
+void shash_table_print(const shash_table_t *ht)
+{
+	shash_node_t *node;
+	int first = 1;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	node = ht->shead;
+
+	while (node != NULL)
+	{
+		if (!first)
+			printf(", ");
+
+		printf("'%s': '%s'", node->key, node->value);
+		first = 0;
+
+		node = node->snext;
+	}
+
+	printf("}\n");
+}
