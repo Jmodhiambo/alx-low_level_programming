0f835ed008571f8c18d0ef5de9d6b4b31eaa9d46
jmodhiambo <jmodhiambo9346@gmail.com>
2025-01-31 11:07:33 +0300

    advance binary search

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -3,50 +3,52 @@
 #include <stdio.h>
 
 /**
- * jump_list - searches for a value in an array of
- * integers using the Jump search algorithm
+ * jump_list - Searches for a value in a sorted singly linked list using Jump Search.
+ * @list: Pointer to the head of the list.
+ * @size: Number of nodes in the list.
+ * @value: The value to search for.
  *
- * @list: input list
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * Return: Pointer to the first node where value is located, or NULL if not found.
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t index, k, m;
-	listint_t *prev;
+    size_t jump_step, pos = 0, multiplier = 0;
+    listint_t *block_start;
 
-	if (list == NULL || size == 0)
-		return (NULL);
+    if (!list || size == 0)
+        return (NULL);
 
-	m = (size_t)sqrt((double)size);
-	index = 0;
-	k = 0;
+    jump_step = (size_t)sqrt((double)size);
 
-	do {
-		prev = list;
-		k++;
-		index = k * m;
+    /* Jumping phase */
+    do {
+        block_start = list;
+        multiplier++;
+        pos = multiplier * jump_step;
 
-		while (list->next && list->index < index)
-			list = list->next;
+        /* Move the list pointer forward */
+        while (list->next && list->index < pos)
+            list = list->next;
 
-		if (list->next == NULL && index != list->index)
-			index = list->index;
+        /* Adjust position if last node is reached */
+        if (!list->next && pos != list->index)
+            pos = list->index;
 
-		printf("Value checked at index [%d] = [%d]\n", (int)index, list->n);
+        printf("Value checked at index [%lu] = [%d]\n", pos, list->n);
 
-	} while (index < size && list->next && list->n < value);
+    } while (pos < size && list->next && list->n < value);
 
-	printf("Value found between indexes ");
-	printf("[%d] and [%d]\n", (int)prev->index, (int)list->index);
+    /* Print the range where value might be */
+    printf("Value found between indexes [%lu] and [%lu]\n", block_start->index, list->index);
 
-	for (; prev && prev->index <= list->index; prev = prev->next)
-	{
-		printf("Value checked at index [%d] = [%d]\n", (int)prev->index, prev->n);
-		if (prev->n == value)
-			return (prev);
-	}
+    /* Linear search within the identified block */
+    while (block_start && block_start->index <= list->index)
+    {
+        printf("Value checked at index [%lu] = [%d]\n", block_start->index, block_start->n);
+        if (block_start->n == value)
+            return (block_start);
+        block_start = block_start->next;
+    }
 
-	return (NULL);
+    return (NULL);
 }
